55cd532290db49f45b0a0c2ae0a6c4e7749d7f23
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:44:35 +0100

    add adapter-core

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 'use strict';
 
 
-const utils      = require(__dirname + '/lib/utils'); // Get common adapter utils
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
 const request    = require('request');
 
 const adapter = utils.Adapter('openweathermap');

6547d751e18c52f5a45e91f446eb093680e5c302
GermanBluefox <dogafox@gmail.com>
2022-08-04 10:57:25 +0200

    Change translations

--- src-widgets/src/App.jsx
+++ src-widgets/src/App.jsx
@@ -4,23 +4,12 @@ import WidgetDemoApp from '@iobroker/vis-2-widgets-react-dev/widgetDemoApp';
 import { i18n as I18n } from '@iobroker/adapter-react-v5';
 
 import Weather from './Weather';
+import translations from './translations';
 
 class App extends WidgetDemoApp {
     constructor(props) {
         super(props);
 
-        const translations = {
-            en: require('./i18n/en.json'),
-            de: require('./i18n/de.json'),
-            ru: require('./i18n/ru.json'),
-            pt: require('./i18n/pt.json'),
-            nl: require('./i18n/nl.json'),
-            fr: require('./i18n/fr.json'),
-            it: require('./i18n/it.json'),
-            es: require('./i18n/es.json'),
-            pl: require('./i18n/pl.json'),
-            'zh-cn': require('./i18n/zh-cn.json'),
-        };
         // init translations
         I18n.extendTranslations(translations);
     }
